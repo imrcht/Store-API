28423474c821903b2dcf2756a8f8284a13f5989a
imrcht <rachchooit@gamil.com>
2021-09-18 12:53:15 +0530

    Database connected to online

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
+const secrets = require("../secrets");
 
 const connectDB = async () => {
-	const conn = await mongoose.connect("mongodb://localhost:27017/StoreDB", {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	const conn = await mongoose.connect(
+		`mongodb+srv://${secrets.user}:${secrets.pwd}@cluster0.vhg7m.mongodb.net/StoreApiDB`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+	);
 
 	console.log(
 		`MongoDb Connected: ${conn.connection.host}`.cyan.underline.bold,
